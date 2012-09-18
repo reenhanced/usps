fac48879683128aa87f8e8efd4c8f5bd40fb44ef
Nicholas Yianilos <npy@reenhanced.com>
2012-09-18 10:23:34 -0400

    added specs for response

---
+++ spec/response/international_shipping_rates_lookup_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe USPS::Response::InternationalShippingRatesLookup do
+
+  it "correctly parses a USPS IntlRateV2 XML response" do
+    response = USPS::Response::InternationalShippingRatesLookup.new(load_xml("international_shipping_rates_lookup.xml"))
+    response.should have(1).packages
+    response.packages.first.tap do |package|
+      package.id.should == '3'
+      package.should have(4).services
+
+      package.services[0].tap do |gxg|
+        gxg.id.should          == '12'
+        gxg.rate.should        == '197.00'
+        gxg.description.should =~ /USPS GXG/
+      end
+
+      package.services[1].tap do |express|
+        express.id.should          == '1'
+        express.rate.should        == '103.00'
+        express.description.should =~ /Express Mail/
+      end
+    end
+  end
+end
